abf8ebe078f68fde0ea8fa62f26c2bbd90b753aa
gabrielTeixeira0021 <charlesgabriel102@gmail.com>
2025-08-25 10:35:40 -0300

    Add initial Java classes and project configuration for serialization

---
+++ AP3_Serialization_prj/src/Carro.java
@@ -0,0 +1,60 @@
+import java.io.Serializable;
+
+public class Carro implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+
+    private String marca;
+    private String modelo; 
+    private String anoModelo;
+    private String placa;
+
+    public Carro (String marca, String modelo, String anoModelo, String placa){
+        super();
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anoModelo = anoModelo;
+        this.placa = placa;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro [marca=" + marca + ", modelo=" + modelo + ", anoModelo=" + anoModelo + ", placa=" + placa + "]";
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getAnoModelo() {
+        return anoModelo;
+    }
+
+    public void setAnoModelo(String anoModelo) {
+        this.anoModelo = anoModelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }    
+}
---
+++ AP3_Serialization_prj/src/Serialization.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.FileInputStream;
+
+public class Serialization {
+    
+    // inicializar uma instancia 
+    Carro car1 = new Carro("BYD", "Dolphin", "2005", "43123123");
+
+    // abrir um arquivo    
+}
+
